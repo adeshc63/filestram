124361076fd2f5d2ee75accb104eb40f9d5590cd
adeshc63 <73774380+adeshc63@users.noreply.github.com>
2025-06-07 06:13:28 +0000

    Add files via upload

---
+++ web/server/__init__.py
@@ -0,0 +1,57 @@
+import logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("aiohttp").setLevel(logging.ERROR)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
+from pyrogram import Client
+from info import *
+from utils import temp
+from typing import Union, Optional, AsyncGenerator
+from pyrogram import types
+from aiohttp import web
+
+# Modified by @marvaldoom
+
+class WebXBot(Client):
+
+    def __init__(self):
+        super().__init__(
+            name=SESSION,
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            workers=50,
+            plugins={"root": "plugins"},
+            sleep_threshold=5,
+        )
+
+    async def set_self(self):
+        temp.BOT = self
+
+# Modified by @marvaldoom
+    
+    async def iter_messages(
+        self,
+        chat_id: Union[int, str],
+        limit: int,
+        offset: int = 0,
+    ) -> Optional[AsyncGenerator["types.Message", None]]:
+        current = offset
+        while True:
+            new_diff = min(200, limit - current)
+            if new_diff <= 0:
+                return
+            messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
+            for message in messages:
+                yield message
+                current += 1
+      
+WebMarvalDoom = WebXBot()
+
+multi_clients = {}
+work_loads = {}
+
+# Modified by @marvaldoom
